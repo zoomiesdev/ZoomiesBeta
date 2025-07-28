f004a0293dc7ac8901f4883686872cdf457945fe
Lili <liannagraham@macbookair.mynetworksettings.com>
2025-07-28 11:07:12 -0400

    Deploy React app to GitHub Pages (clean root first)

--- src/pages/AmbassadorProfilePage.jsx
+++
@@ -1,4 +0,0 @@
-import StompyBanner from "../assets/StompyBanner.png";
-import StompyProfilePic from "../assets/StompyProfilePic.png";
-import LunaBanner from "../assets/LunaBanner.png";
-import LunaProfilePic from "../assets/LunaProfilePic.png"; 
